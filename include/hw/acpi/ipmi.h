d13a98bc5f278c4c9e5fb9b1d60619b5f1655aed
Corey Minyard <cminyard@mvista.com>
2013-05-23 16:58:30 -0500

    acpi: Add IPMI table entries
    Use the new ACPI table construction tools to create an ACPI
    entry for IPMI.  This adds a function called from build_dsdt
    to add an DSDT entry for IPMI if IPMI is compiled in and has
    registered firmware.  It also adds a dummy function if IPMI
    is not compiled in.
    
    This conforms to section "C3-2 Locating IPMI System Interfaces in
    ACPI Name Space" in the IPMI 2.0 specification.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ include/hw/acpi/ipmi.h
@@ -0,0 +1,17 @@
+/*
+ * QEMU IPMI ACPI handling
+ *
+ * Copyright (c) 2015 Corey Minyard <cminyard@mvista.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+#ifndef HW_ACPI_IPMI_H
+#define HW_ACPI_IPMI_H
+
+#include "qemu/osdep.h"
+#include "hw/acpi/aml-build.h"
+
+void acpi_add_ipmi(Aml *ssdt);
+
+#endif /* HW_ACPI_IPMI_H */
