fd952d77b13be4fd5a96b3aa7981fe90f45aa0a1
Corey Minyard <cminyard@mvista.com>
2013-05-23 16:58:30 -0500

    acpi: Add IPMI table entries
    Use the new ACPI table construction tools to create an ACPI
    entry for IPMI.  This adds a function called from build_ssdt
    to add an SSDT entry for IPMI if IPMI is compiled in and has
    registered firmware.  It also adds a dummy function if IPMI
    is not compiled in.
    
    This conforms to section "C3-2 Locating IPMI System Interfaces in
    ACPI Name Space" in the IPMI 2.0 specification.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

---
+++ hw/acpi/noipmi.c
@@ -0,0 +1,14 @@
+/*
+ * IPMI ACPI firmware handling
+ *
+ * Copyright (c) 2015 Corey Minyard, MontaVista Software, LLC
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ */
+
+#include "hw/acpi/ipmi.h"
+
+void acpi_add_ipmi(Aml *ssdt)
+{
+}
