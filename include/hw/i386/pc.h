94a013794125188023f02c6bccc19d88cceec712
Corey Minyard <cminyard@mvista.com>
2016-05-12 20:43:45 -0500

    pc: Add an SMB0 ACPI device to q35
    This is so I2C devices can be found in the ACPI namespace.  Currently
    that's only IPMI, but devices can be easily added now.
    
    Adding the devices required some PCI information, and the bus itself
    to be added to the PCMachineState structure.
    
    Note that this only works on Q35, the ACPI for PIIX4 is not capable
    of handling an SMBus device.
    
    Signed-off-by: Corey Minyard <cminyard@mvista.com>

--- include/hw/i386/pc.h
+++ include/hw/i386/pc.h
@@ -39,6 +39,7 @@ struct PCMachineState {
     HotplugHandler *acpi_dev;
     ISADevice *rtc;
     PCIBus *bus;
+    I2CBus *smbus;
     FWCfgState *fw_cfg;
     qemu_irq *gsi;
 
@@ -122,6 +123,7 @@ struct PCMachineClass {
     bool rsdp_in_ram;
     int legacy_acpi_table_size;
     unsigned acpi_data_size;
+    bool do_not_add_smb_acpi;
 
     /* SMBIOS compat: */
     bool smbios_defaults;
